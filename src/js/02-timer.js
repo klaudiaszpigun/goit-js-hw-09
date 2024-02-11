3c6f4812c72db46682da8ed83ab87385b1de79d0
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-11 01:08:36 +0100

    upd

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,37 +1,3 @@
-/*
-  1. biblioteka flatpickr
-  2. flatpickr(selector, options) =>
-  3. const options = {
-      enableTime: true,
-      time_24hr: true,
-      defaultDate: new Date(),
-      minuteIncrement: 1,
-      onClose(selectedDates) {
-        console.log(selectedDates[0]);
-      },
-    };
-  4. selectedDates => tablica wybranych dat, dlatego bierzemy z niej pierwszy element.
-  5. onClose() => zamknięcie kalendarza
-  6. window.alert("Please choose a date in the future")
-  7. function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-    return { days, hours, minutes, seconds };
-  }
-  console.log(convertMs(2000));// {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000));// {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000));// {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-  8. addLeadingZero(value) => padStart()
-
-*/
-
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
